a7605d3152da3609d3e7ec3ab6e74aec7fdf24ce
ysfzxz <bbbmaroc@gmail.com>
2024-06-06 16:12:07 +0100

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ Module to the to_kv func
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int | float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int | float], ) -> Tuple[str, float]:
     """
     func to_kv that takes k and v as parms
     return:
