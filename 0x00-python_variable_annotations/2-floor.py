142aff39eacd303bbae3be6d937c67aedd39f9f8
ysfzxz <bbbmaroc@gmail.com>
2024-06-06 15:17:38 +0100

    tas2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+"""
+This is a Module of the floor func
+"""
+
+
+def floor(a: float) -> int:
+    """
+    The floor func that change a to an int
+    """
+
+    return math.floor(a)
