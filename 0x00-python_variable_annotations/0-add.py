3d1ee28e9cfd01a16aa59da322d680de1344ed36
ysfzxz <bbbmaroc@gmail.com>
2024-06-06 15:05:19 +0100

    file1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 """
-  Module of the add function
+    Module of the add function
 """
 
 
 def add(a: float, b: float) -> float:
-  """
+    """
     function that adds two flowting numbers a and b
 
     return:
       It return the sum of a and b
-  """
+    """
 
-  return a + b
+    return a + b
