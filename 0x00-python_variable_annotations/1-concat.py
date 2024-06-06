2e4b312a8bd4faffb8dbc6797c28dde89497db38
ysfzxz <bbbmaroc@gmail.com>
2024-06-06 15:10:22 +0100

    tqsk1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+A function that concat two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This func concat two strings
+    return:
+      the concat of the two strings ...
+    """
+    return str1+str2
