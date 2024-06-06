ce4ad7194a44bb1e6daf7b3f6a5c4459d0eee248
ysfzxz <bbbmaroc@gmail.com>
2024-06-06 13:34:45 +0100

    file1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+  module of the add function
+"""
+
+def add(a: float, b: float):
+  """
+    function that add two variables a and b
+    return:
+      it return the sum of a and b
+  """
+  return a + b
