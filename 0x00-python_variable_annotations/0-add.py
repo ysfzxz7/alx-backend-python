4128fca65d5f2e7fc21dc9bf61a83cd58772254a
ysfzxz <bbbmaroc@gmail.com>
2024-06-06 14:53:35 +0100

    file1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """
-  module of the add function
+  Module of the add function
 """
+
 
 def add(a: float, b: float) -> float:
   """
-    function that add two variables a and b
+    function that adds two flowting numbers a and b
+    
     return:
-      it return the sum of a and b
+      It return the sum of a and b
   """
+  
   return a + b
