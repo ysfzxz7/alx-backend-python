931a755f76c94f69c3a83705e4676f2e97ff91cd
ysfzxz <bbbmaroc@gmail.com>
2024-06-10 16:07:10 +0100

    task1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    This is the Module of the waitRandom func
+"""
+
+
+import random
+import asyncio
+
+async def wait_random (max_delay: int = 10) -> float:
+    """
+    wait_random func take one opt parameter max_delay
+        return the delay generated otherwise 10,
+    """
+    delay: float = random.uniform(0,max_delay)
+    await asyncio.sleep(delay)
+    return delay
